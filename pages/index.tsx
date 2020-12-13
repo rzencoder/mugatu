b2e4c3431b823bcf26154b25b002dfb538da53e2
adam <20361733+rzencoder@users.noreply.github.com>
2020-12-13 14:21:09 +0000

    eslint, prettier setup

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Hello</main>
+    </div>
+  )
+}
